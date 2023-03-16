8b05f0c70c26c6c1920783f9e375c06b63a705c4
anyeleventura <anyeleventuralima@gmail.com>
2023-03-15 21:51:38 -0300

    atualiza estilo do rodapé

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,36 +17,39 @@ export default function Footer()
                         <div><img alt="logo da Digital College" src={Logodc}/></div>
                         <div className="logonome"><h3>Digital Store</h3></div>
                     </div>
-                    <Paragrafo class="paragrafos paraabout" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."/>
+                    <Paragrafo class="paraabout" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."/>
                     <a href='https://www.facebook.com'><img alt="logo do Facebook" className="icon" src={Facebook}/></a>
                     <a href='https://www.instagram.com'><img alt="logo do Instagram" className="icon" src={Instagram}/></a>
                     <a href='https://twitter.com/home'><img alt="logo do Twitter" className="icon" src={Twitter}/></a>
                 </div>
                 <div className="column">
-                    <h4>Informações</h4>
-                    <a href='/'><Paragrafo class="paragrafos" text="Sobre Drip Store"/></a>
-                    <a href='/'><Paragrafo class="paragrafos" text="Segurança"/></a>
-                    <a href='/'><Paragrafo class="paragrafos" text="Wishlist"/></a>
-                    <a href='/'><Paragrafo class="paragrafos" text="Blog"/></a>
-                    <a href='/'><Paragrafo class="paragrafos" text="Trabalhe conosco"/></a>
-                    <a href='/'><Paragrafo class="paragrafos" text="Meus pedidos"/></a>
+                    <h4>Informação</h4>
+                    <a href='/'><Paragrafo text="Sobre Drip Store"/></a>
+                    <a href='/'><Paragrafo text="Segurança"/></a>
+                    <a href='/'><Paragrafo text="Wishlist"/></a>
+                    <a href='/'><Paragrafo text="Blog"/></a>
+                    <a href='/'><Paragrafo text="Trabalhe conosco"/></a>
+                    <a href='/'><Paragrafo text="Meus pedidos"/></a>
                 </div>
-                <div className="column">
+                <div className="column category">
                     <h4>Categorias</h4>
-                    <a href='/'><Paragrafo class="paragrafos link" text="Camisetas"/></a>
-                    <a href='/'><Paragrafo class="paragrafos link" text="Calças"/></a>
-                    <a href='/'><Paragrafo class="paragrafos link" text="Bonés"/></a>
-                    <a href='/'><Paragrafo class="paragrafos link" text="Headphones"/></a>
-                    <a href='/'><Paragrafo class="paragrafos link" text="Tênis"/></a>
+                    <a href='/'><Paragrafo text="Camisetas"/></a>
+                    <a href='/'><Paragrafo text="Calças"/></a>
+                    <a href='/'><Paragrafo text="Bonés"/></a>
+                    <a href='/'><Paragrafo text="Headphones"/></a>
+                    <a href='/'><Paragrafo text="Tênis"/></a>
                 </div>
                 <div className="column">
                     <h4>Contato</h4>
-                    <Paragrafo class="paragrafos .paraabout" text="Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161"/>
-                    <Paragrafo class="paragrafos .paraabout" text="(85) 3051-3411"/>
+                    <Paragrafo class="paragrafo" text="Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161"/>
+                    <Paragrafo class="paragrafo" text="(85) 3051-3411"/>
                 </div>
             </div>
+            
             <div className="creditos">
+                <div>
                 <Paragrafo class="patente" text="@ 2022 Digital College"/>
+                </div>
             </div>
         </footer>
         
