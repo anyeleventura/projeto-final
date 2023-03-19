2b14b4e9f764fab78b656cba4cc65d2ec4f17943
anyeleventura <anyeleventuralima@gmail.com>
2023-03-19 14:45:53 -0300

    testando branch

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import SectionSix from '../../components/Sectionsix';
+import './style.css';
+export default function Home() 
+{
+    return(
+        <>
+        <Header/>
+        <SectionSix/>
+        <Footer/>
+        </>
+    )
+}
---
+++ src/routes/Ways.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DetalhesProduto from '../pages/DetalhesProduto';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
+export default function Ways()
+{
+    return(
+        <>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/produtos' element="pagina de produtos"/>
+                <Route path='/produtos/:id' element={<DetalhesProduto/>}/>
+                <Route path='/categorias' element="pagina de categorias"/>
+                <Route path='/meus-pedidos' element="pagina de meus pedidos"/>
+                
+                <Route path='/*' element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+        </>
+    )
+}
