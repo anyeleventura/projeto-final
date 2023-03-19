07ddf38675c7d9ee5f49311e2c920cbe60faa0ba
unknown <rosanadeveloper@gmail.com>
2023-03-19 18:58:59 -0300

    Alteracoes para testar merge

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-// import Banner from "./components/Banner";
-
 import React from 'react';
 import './App.scss';
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-
-import { Container } from "@mui/material";
-
-import Home from "./pages/Home";
+import Ways from './routes/Ways';
 
 
-export default function App() {
-  return (
+export default function App() 
+{
+  return(
     <>
-      <Home />
+      <Ways/>
     </>
-  );
+  )
 }
