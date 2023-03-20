0a5e0e0950aac1142dccc2af76117b6c8d992f23
Anyele Ventura <124682837+anyeleventura@users.noreply.github.com>
2023-03-20 00:37:41 -0300

    modifica importação

--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -1,12 +1,12 @@
 import Header from '../../components/Header';
-import BuscarProdutos from '../../components/BuscarProdutos';
+import BuscarProduto from '../../components/BuscarProduto';
 import Footer from '../../components/Footer';
 export default function Produtos() 
 {
     return(
         <>
         <Header/>
-        <BuscarProdutos/>
+        <BuscarProduto/>
         <Footer/>
         </>
     )
