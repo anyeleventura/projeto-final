2b3b5ae4bf8aaa4de04a9033f7f2f664f120a68a
unknown <taianeandrademtba@gmail.com>
2023-04-09 00:43:57 -0300

    teste

---
+++ src/pages/Produtos/index.js
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import BuscarProduto from '../../components/BuscarProduto';
+import Footer from '../../components/Footer';
+export default function Produtos() 
+{
+    return(
+        <>
+        <Header/>
+        <BuscarProduto/>
+        <Footer/>
+        </>
+    )
+}
